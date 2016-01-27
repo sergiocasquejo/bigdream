0806490111e89b4e9865ae2076db89fa7b153b73
sergiocasquejo <serg.casquejo@yahoo.com>
2016-01-27 17:49:33 +0800

    Update header.php

--- wp-content/themes/cebu-dream/templates/header.php
+++ wp-content/themes/cebu-dream/templates/header.php
@@ -1,3 +1,37 @@
+<div class="header_top">
+  <div class="container">
+      <div class="header_left float-left">
+          <span><i class="hillter-icon-cloud"></i> 18 °C</span>
+          <span><i class="hillter-icon-location"></i> 225 Beach Street, Australian</span>
+          <span><i class="hillter-icon-phone"></i> 1-548-854-8898</span>
+      </div>
+      <div class="header_right float-right">
+  
+          <span class="login-register">
+              <a href="login.html">Login</a>
+              <a href="register.html">register</a>
+          </span>
+  
+          <div class="dropdown currency">
+              <span>USD <i class="fa fa"></i></span>
+              <ul>
+                  <li class="active"><a href="#">USD</a></li>
+                  <li><a href="#">EUR</a></li>
+              </ul>
+          </div>
+  
+          <div class="dropdown language">
+              <span>ENG</span>
+  
+              <ul>
+                  <li class="active"><a href="#">ENG</a></li>
+                  <li><a href="#">FR</a></li>
+              </ul>
+          </div>
+  
+      </div>
+  </div>
+  </div>
 <!-- start header -->
 <div class="navbar navbar-top navbar-fixed-top">
   <div class="container nav-mobile">
