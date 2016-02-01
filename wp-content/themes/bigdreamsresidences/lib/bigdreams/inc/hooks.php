6433e6863f5806be98b7f021626652945b9a3305
sergiocasquejo <serg.casquejo@yahoo.com>
2016-02-01 11:25:49 +0800

    Update hooks.php

--- wp-content/themes/bigdreamsresidences/lib/bigdreams/inc/hooks.php
+++ wp-content/themes/bigdreamsresidences/lib/bigdreams/inc/hooks.php
@@ -19,3 +19,23 @@ function bdr_login_redirect( $redirect_to, $request, $user ) {
 }
 
 add_filter('login_redirect', 'bdr_login_redirect', 10, 3);
+
+add_filter('site_url',  'wplogin_filter', 10, 3);
+function wplogin_filter( $url, $path, $orig_scheme )
+{
+ $old  = array( "/(wp-login\.php)/");
+ $new  = array( "login");
+ return preg_replace( $old, $new, $url, 1);
+}
+
+
+// Change WordPress Login Logo URL
+add_filter( 'login_headerurl', 'custom_logo_url' );
+function custom_logo_url($url) {
+ return get_bloginfo('url');
+}
+
+add_filter('login_headertitle', 'custom_logo_title');
+function custom_logo_title() {
+    return get_bloginfo('name');
+}
