a2564dec9e5000639b7ef199083c04c6b1c37dee
sergiocasquejo <serg.casquejo@yahoo.com>
2016-02-11 10:46:19 +0800

    Update guest_calendar.html.php

--- wp-content/themes/bigdreamsresidences/lib/core/views/guest_calendar.html.php
+++ wp-content/themes/bigdreamsresidences/lib/core/views/guest_calendar.html.php
@@ -1,23 +1,23 @@
 <div class="wrap">
 	<h1>Guest Calendar</h1>
 	<?php site_notices(); ?>
-	<div id="guestCalendarWrapper">
+	div id="guestCalendarWrapper">
 		<table class="widefat">
 			<thead>
 				<tr>
 					<th width="10%"></th>
-					<th colspan="<?php echo  ( $total_day - 1  ) * 2; ?>"><?php echo format_date( $selected_date, 'F Y' ); ?></th>
+					<th colspan="<?php echo ( $total_day * 2 ) - 2; ?>"><?php echo format_date( $selected_date, 'F Y' ); ?></th>
 				</tr>
 				<tr>
 					<th width="10%"></th>
-					<?php $date = $selected_date; for ( $start = $start_date; $start <= $total_day; $start++ ) { ?>
-					<th colspan="<?php echo $start != $start_date && $start != $total_day ? '2' : ''; ?>"><?php echo format_date( $date, 'j' ); ?></th>
+					<?php $date = $selected_date;  while (strtotime($date) <= strtotime($end_date)) { ?>
+					<th colspan="<?php echo $date != $selected_date && $date != $end_date ? '2' : ''; ?>"><?php echo format_date( $date, 'j' ); ?></th>
 					<?php $date = add_days_to_date( $date, 1 ); } ?>
 				</tr>
 				<tr>
 					<th width="10%"></th>
-					<?php $date = $selected_date; for ( $start = $start_date; $start <= $total_day; $start++ ) { ?>
-					<th colspan="<?php echo $start != $start_date && $start != $total_day ? '2' : ''; ?>"><?php echo format_date( $date, 'D' ); ?></th>
+					<?php $date = $selected_date;  while (strtotime($date) <= strtotime($end_date)) { ?>
+					<th colspan="<?php echo $date != $selected_date && $date != $end_date ? '2' : ''; ?>"><?php echo format_date( $date, 'D' ); ?></th>
 					<?php $date = add_days_to_date( $date, 1 ); } ?>
 				</tr>
 			</thead>
@@ -29,14 +29,14 @@
 				?>
 				<tr>
 					<td><?php echo $k; ?></td>
-					<td colspan="<?php echo  ( $total_day - 1 ) * 2; ?>" class="calendar_row <?php echo $total_day; ?>">
+					<td colspan="<?php echo  ( $total_day * 2 ) - 2; ?>" class="calendar_row <?php echo $total_day; ?>">
 						<div class="row_data">
 							<div class="bg_row">
 								<table>
 									<tbody>
-										<?php for ( $i = $start_date; $i <= $total_day - 1; $i++ ) { ?>
+										<?php $date = $selected_date;  while (strtotime($date) < strtotime($end_date)) { ?>
 										<td colspan="2"></td>
-										<?php } ?>
+										<?php $date = add_days_to_date( $date, 1 );} ?>
 									</tbody>
 								</table>
 							</div>
@@ -46,21 +46,17 @@
 										<?php 
 										$b = $selected_date;
 										foreach ( $c as $cal ) {
-											if ( strtotime( $cal['from'] ) > strtotime( $end_date ) ) continue;
+											if ( strtotime( $cal['from'] ) > strtotime( $end_date ) || strtotime( $cal['from'] ) < strtotime( $selected_date ) ) continue;
 											
-
 											if ( ( $f =  count_days_gap( $b, $cal['from'], $end_date ) ) > 0 ) {
 												echo '<td colspan="'. $f .'"></td>';
 												$b = add_days_to_date( $b, $f );
 											}
-
 											$f = count_days_gap( $cal['from'], $cal['to'], $end_date );
 											echo '<td colspan="'. $f .'"><div class="text">'. $cal['guest'] .'</div></td>';
 											$b = $cal['to'] > $end_date ? $end_date : $cal['to'];
-
 										}
-
-										if ( strtotime($end_date) > strtotime($b) && ( $f = count_days_gap( $b, $end_date, $end_date ) - 1  ) > 0 ) {
+										if ( strtotime($end_date) > strtotime($b) && ( $f = count_days_gap( $b, $end_date, $end_date )  ) > 0 ) {
 											echo '<td colspan="'. $f .'"></td>';
 										}
 										?>
